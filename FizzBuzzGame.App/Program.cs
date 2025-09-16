afb75b45c3c30fde125308d2a852e38fc5206f9b
IvanMaruna <ivan.maruna21@mail.com>
2025-09-16 19:23:34 +0200

    Del 1 av matematikkleken FizzBuzz

--- FizzBuzzGame.App/Program.cs
+++ FizzBuzzGame.App/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FizzBuzzGame.Core;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fizzBuzzDict = new Dictionary<string, int>
+        {
+            { "Fizz", 3 },
+            { "Buzz", 5 }
+        };
+
+        var fizzBuzz = new FizzBuzz(fizzBuzzDict);
+
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(fizzBuzz.GetValue(i));
+        }
+    }
+}
--- FizzBuzzGame.Tests/FizzBuzzTests.cs
+++ FizzBuzzGame.Tests/FizzBuzzTests.cs
@@ -1,10 +1,38 @@
-﻿namespace FizzBuzzGame.Tests;
+﻿using FizzBuzzGame.Core;
+
+namespace FizzBuzzGame.Tests;
 
 public class FizzBuzzTests
 {
     [Fact]
-    public void Test1()
+    public void ReturnsFizzForMultiplesOf3()
     {
+        var rules = new Dictionary<string, int> { { "Fizz", 3 }, { "Buzz", 5 } };
+        var fizzBuzz = new FizzBuzz(rules);
+        Assert.Equal("Fizz", fizzBuzz.GetValue(3));
+    }
 
+    [Fact]
+    public void ReturnsBuzzForMultiplesOf5()
+    {
+        var rules = new Dictionary<string, int> { { "Fizz", 3 }, { "Buzz", 5 } };
+        var fizzBuzz = new FizzBuzz(rules);
+        Assert.Equal("Buzz", fizzBuzz.GetValue(5));
+    }
+
+    [Fact]
+    public void ReturnsFizzBuzzForMultiplesOf3And5()
+    {
+        var rules = new Dictionary<string, int> { { "Fizz", 3 }, { "Buzz", 5 } };
+        var fizzBuzz = new FizzBuzz(rules);
+        Assert.Equal("FizzBuzz", fizzBuzz.GetValue(15));
+    }
+
+    [Fact]
+    public void ReturnsNumberForNonMultiples()
+    {
+        var rules = new Dictionary<string, int> { { "Fizz", 3 }, { "Buzz", 5 } };
+        var fizzBuzz = new FizzBuzz(rules);
+        Assert.Equal("2", fizzBuzz.GetValue(2));
     }
 }
