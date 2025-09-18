1cd86b554564008521788ae1d38ebcdfdc1c28a5
Thomas Leela <thomas.leela@epinova.no>
2025-09-18 07:54:14 +0200

    Start master with clean console app.

--- FizzBuzzGame.App/Program.cs
+++ FizzBuzzGame.App/Program.cs
@@ -1,20 +1,7 @@
-﻿using FizzBuzzGame.Core;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        var fizzBuzzDict = new Dictionary<string, int>
-        {
-            { "Fizz", 3 },
-            { "Buzz", 5 }
-        };
-
-        var fizzBuzz = new FizzBuzz(fizzBuzzDict);
-
-        for (int i = 1; i <= 100; i++)
-        {
-            Console.WriteLine(fizzBuzz.GetValue(i));
-        }
+        Console.WriteLine("Hello world");
     }
 }
